bb82eea8f9d891dbca5d9bbb8cb0c25098d37a77
Guilherme Guelere <guilhermeg.dev@gmail.com>
2023-10-05 22:11:00 -0300

    Add files via upload

--- form-gamificado/script.js
+++ form-gamificado/script.js
@@ -7,6 +7,7 @@ function calcularProgresso() {
     camposObrigatorios.forEach(function (campo) {
         if (campo.value.trim() !== '') {
             camposPreenchidos++;
+            
         }
     });
 
@@ -14,6 +15,9 @@ function calcularProgresso() {
 
     // Atualizar o valor da barra de progresso
     document.getElementById('progresso').value = progresso;
+    if (progresso>=100){
+        progresso.classList.add('explodir');
+    }
 }
 
 // Chamar a função sempre que um campo for preenchido ou perder o foco
@@ -22,3 +26,23 @@ campos.forEach(function (campo) {
     campo.addEventListener('input', calcularProgresso);
     campo.addEventListener('blur', calcularProgresso);
 });
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+        // Aguarde até que o conteúdo da página seja carregado
+    
+        // Mostre a tela de saudação
+        const telaDeSaudacao = document.querySelector(".tela-de-saudacao");
+        telaDeSaudacao.style.opacity = "1";
+    
+        // Oculte a tela de saudação após um atraso (por exemplo, 3 segundos)
+        setTimeout(function () {
+            telaDeSaudacao.style.opacity = "0";
+            telaDeSaudacao.style.display = "none";
+    
+            // Mostre o conteúdo do site
+            const conteudo = document.querySelector(".conteudo");
+            conteudo.style.display = "block";
+        }, 3000); // 3 segundos
+    });
+    
